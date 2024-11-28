9f8a574b2c05c4dfadc9269453d905a55d901019
John Stuart Phillips <0q5av15l@students.codeinstitute.net>
2024-11-28 21:02:26 +0000

    Add the questions

--- run.py
+++ run.py
@@ -1,3 +1,86 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+class Options:
+    """Defines the options"""
+    def __init__(self, number, country, capital, incorrect_1, incorrect_2):
+        self.number = number
+        self.country = country
+        self.capital = capital
+        self.incorrect_1 = incorrect_1
+        self.incorrect_2 = incorrect_2 
+    
+    def the_question(self):
+        """the question"""
+        print(f"Question {self.number}: What is the capital of {self.country}?")
+        options = [self.capital, self.incorrect_1, self.incorrect_2]
+        random.shuffle(options)
+        print('A: ', options[0])
+        print('B: ', options[1])
+        print('C: ', options[2])
+        return options
+    
+def content():
+    """Country, capital and two incorrect options stored here"""
+    dictionary = [
+        {
+            'country': 'the United States', 'capital': 'Washington DC', 'incorrect_1': 'Los Angeles', 'incorrect_2': 'New York',
+        },
+        {
+            'country': 'China', 'capital': 'Beijing', 'incorrect_1': 'Hong Kong', 'incorrect_2': 'Shanghai',
+        },
+        {
+            'country': 'Germany', 'capital': 'Berlin', 'incorrect_1': 'Hamburg', 'incorrect_2': 'Munich',
+        },
+        {
+            'country': 'Japan', 'capital': 'Tokyo', 'incorrect_1': 'Hiroshima', 'incorrect_2': 'Osaka',
+        },
+        {
+            'country': 'India', 'capital': 'New Dheli', 'incorrect_1': 'Chennai', 'incorrect_2': 'Mumbai',
+        },
+        {
+            'country': 'the United Kindgom', 'capital': 'London', 'incorrect_1': 'Birmingham', 'incorrect_2': 'Manchester',
+        },
+        {
+            'country': 'France', 'capital': 'Paris', 'incorrect_1': 'Lyon', 'incorrect_2': 'Nice',
+        },
+        {
+            'country': 'Italy', 'capital': 'Rome', 'incorrect_1': 'Milan', 'incorrect_2': 'Naples',
+        },
+        {
+            'country': 'Canada', 'capital': 'Ottawa', 'incorrect_1': 'Montreal', 'incorrect_2': 'Toronto',
+        },
+        {
+            'country': 'Brazil', 'capital': 'Brasilia', 'incorrect_1': 'Rio de Janerio', 'incorrect_2': 'Sao Paulo',
+        }
+    ]
+    
+    score = 0
+
+    for index, question in enumerate(dictionary):
+        question = Options(
+            number=index + 1,
+            country=question["country"],
+            capital=question["capital"],
+            incorrect_1=question["incorrect_1"],
+            incorrect_2=question["incorrect_2"]
+        )
+        options = question.the_question()
+        while True:
+            available_options = input("Please select and option of A, B or C, then press Enter:\n")
+            if available_options.upper() not in ['A', 'B', 'C']:
+                print("That is not a valid answwer, please select A, B or C")
+            else:
+                break
+
+        if available_options.upper() == "A" and options[0] == question.capital:
+            print("Well done, that is the correct answer")
+            score += 1
+        elif available_options.upper() == "B" and options[1] == question.capital:
+            print("Well done, that is the correct answer")
+            score += 1
+        elif available_options.upper() == "C" and options[2] == question.capital:
+            print("Well done, that is the correct answer")
+            score += 1
+        else:
+            print("Sorry, you have chosen an incorrect option")
+            print("The correct answer is", capital)
